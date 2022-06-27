401132faf8baa7641ecd3aa80ef051e4418ca792
Jess <jssanji03@gmail.com>
2022-06-27 16:46:07 +0800

    新增折讓單

--- dist/js/component.js
+++ dist/js/component.js
@@ -28,6 +28,7 @@ $(function () {
     }
   );
   $('.datatable-RWDcard').DataTable({
+      
       "responsive": false,
       "lengthChange": false,
       "autoWidth": false,
@@ -53,6 +54,29 @@ $(function () {
         },
     }
   );
+  $('.datatable-invoice').DataTable({
+      
+      "responsive": false,
+      "lengthChange": false,
+      "autoWidth": false,
+      "searching": false,
+      "paging": false,
+      "info": false,
+      // "scrollX": true,
+      "ordering": false,
+      "language": {
+            "info": "顯示 _PAGE_ 至 _PAGES_",
+            "search": "搜尋 :",
+            "paginate": {
+                "previous": "上一頁",
+                "next":"下一頁"
+            },
+            "lengthMenu": "顯示 _MENU_ 筆資料"
+      },
+        "initComplete": function (settings, json) {
+            $(".datatable-RWDcard").wrap("<div style='overflow:auto; width:100%;position:relative;'></div>");
+        },
+    }
+  );
 
 });
-  
